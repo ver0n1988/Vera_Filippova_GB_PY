80039658a23f0b6937f5db1fc8ac3913f41ba33c
ver0n1988 <104308690+ver0n1988@users.noreply.github.com>
2022-06-06 12:47:15 +0300

    add files

---
+++ task_4.2.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def currency_rates(cur_code="", url="http://www.cbr.ru/scripts/XML_daily.asp"):
+    if not (cur_code and url):
+        return None
+
+    cur_code = cur_code.upper()
+    req = requests.get(url)
+
+    if req.ok:
+        cur = req.text.split(cur_code)
+
+        if len(cur) == 1:
+            return None
+
+        value = cur[1].split("</Value>")[0].split("<Value>")[1]
+        value = float(value.replace(",", "."))
+
+        return (value)
+
+    else:
+        return None
+
+
+print((currency_rates("USD")))
+print((currency_rates("eur")))

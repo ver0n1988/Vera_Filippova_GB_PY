a8cb68dd921c6cfe22049dbe1fb5effc40acd98c
ver0n1988 <104308690+ver0n1988@users.noreply.github.com>
2022-06-22 21:28:58 +0300

    homework#9

---
+++ task_9.2.py
@@ -0,0 +1,22 @@
+class Road:
+
+    _length: float
+    _width: float
+
+    def __init__(self, length: float = 0, width: float = 0) -> None:
+        self._length = length
+        self._width = width
+
+    def calc(self, density: float, thickness: float) -> float:
+        return self._length * self._width * density * thickness
+
+
+if __name__ == "__main__":
+    road = Road(length=20, width=5000)
+    print(road.calc(25, 5))
+
+    # or if we in module
+    road = Road()
+    road._length = 20
+    road._width = 5000
+    print(road.calc(density=25, thickness=5))
